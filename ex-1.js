e20d3b4bc9a77d6be92cbca9a3f938dca5a1b052
Kanompang <suparadajk21@gmail.com>
2024-09-15 16:11:04 +0700

    BP5-Kanompang

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,13 @@
 // Exercise #1: Food Order
 
 // Start coding here
+const foodOrder = {};
+let orderNumber = "A0234";
+let address = "Bangkok";
+let paymentType = "Cash";
+let totalPrice = 4500;
+let restaurantName = "MK";
+foodOrder.totalPrice = 5000;
+foodOrder.paymentType = "Credit Card";
+
+console.log(foodOrder);
